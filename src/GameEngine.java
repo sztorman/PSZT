daa5edddea65ef4d34911547edd7516588746240
mateuszklr <a>
2014-01-27 00:02:34 +0100

    Poprawione indeksy

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -13,24 +13,20 @@ public class GameEngine implements ValueFunction {
 
 
     public int checkValueOfArea(int x, int y, Board board){
-       Board localBoard = board;
-       int black = 0;
-       int white = 0;
+        int black = 0;
+        int white = 0;
         int newValue = 0;
         int y1;
-       int valueTemp;
+        int valueTemp;
         //sprawdzanie tablicy po skosie
         y1 = y-3;
-        for (int i = x-3;i<x+3;i++){
-
-                if ((i>=0 && y1>=0) && (i< 10 && y1 < 10)){
-                    if (localBoard.getSquare(i,y1).getState().equals(Square.State.BLACK)) black++;
-                    else if (localBoard.getSquare(i,y1).getState().equals(Square.State.WHITE)) white++;
-                }
-
-            y1++;
-
+        for (int i = x-3; i <= x+3; i++){
+            if ((i>=0 && y1>=0) && (i< 10 && y1 < 10)){
+                if (board.getSquare(i, y1).getState().equals(Square.State.BLACK)) black++;
+                else if (board.getSquare(i, y1).getState().equals(Square.State.WHITE)) white++;
             }
+            y1++;
+        }
 
         getValueFunction(white, black);
         black = 0;
@@ -38,15 +34,12 @@ public class GameEngine implements ValueFunction {
 
         //sprawdzanie po drugim skosie
         y1 = y+3;
-        for (int i = x-3;i<x+3;i++){
-
+        for (int i = x-3; i <= x+3; i++){
             if ((i>=0) && (y1>=0) && (i<10) && (y1 < 10)){
-                if (localBoard.getSquare(i,y1).getState().equals(Square.State.BLACK)) black++;
-                else if (localBoard.getSquare(i,y1).getState().equals(Square.State.WHITE)) white++;
+                if (board.getSquare(i, y1).getState().equals(Square.State.BLACK)) black++;
+                else if (board.getSquare(i, y1).getState().equals(Square.State.WHITE)) white++;
             }
-
             y1--;
-
         }
 
         getValueFunction(white, black);
@@ -54,27 +47,22 @@ public class GameEngine implements ValueFunction {
         white = 0;
 
         //sprawdzanie góra - dół o lewo - prawo
-
-        for (int i = x-3;i<x+3;i++){
-
+        for (int i = x-3; i <= x+3; i++){
             if ((i>=0 && y>=0) && (i<10 && y < 10)){
-                if (localBoard.getSquare(i,y).getState().equals(Square.State.BLACK)) black++;
-                else if (localBoard.getSquare(i,y).getState().equals(Square.State.WHITE)) white++;
+                if (board.getSquare(i, y).getState().equals(Square.State.BLACK)) black++;
+                else if (board.getSquare(i, y).getState().equals(Square.State.WHITE)) white++;
             }
-
         }
 
         getValueFunction(white, black);
         black = 0;
         white = 0;
 
-        for (int i = y-3;i<y+3;i++){
-
+        for (int i = y-3; i <= y+3; i++){
             if ((x>=0 && i>=0) && (x<10 && i < 10)){
-                if (localBoard.getSquare(x,i).getState().equals(Square.State.BLACK)) black++;
-                else if (localBoard.getSquare(x,i).getState().equals(Square.State.WHITE)) white++;
+                if (board.getSquare(x, i).getState().equals(Square.State.BLACK)) black++;
+                else if (board.getSquare(x, i).getState().equals(Square.State.WHITE)) white++;
             }
-
         }
 
         getValueFunction(white, black);
@@ -82,7 +70,6 @@ public class GameEngine implements ValueFunction {
         white = 0;
 
         return value;
-
     }
 
 
@@ -103,7 +90,6 @@ public class GameEngine implements ValueFunction {
        // else if (black<3) valueTemp = 0;
 
         value += valueTemp;
-
     }
 
     public int[] makeMove(Game game){
@@ -121,58 +107,48 @@ public class GameEngine implements ValueFunction {
         int empty = 0;
         int filled = 0;
 
-        for (int i = lastX-1;i<lastX+1;i++)
-            for (int j=lastY-1;j<lastY+1;j++){
-                if (i<0|| i>10 || j<0 || j>10 || board.getSquare(i,j).getState().equals(Square.State.BLACK) || board.getSquare(i,j).getState().equals(Square.State.WHITE) ){
-
+        // Policz zajęte i wolne pola w okolicy ostatnio polozonego pionka
+        for (int i = lastX-1; i<=lastX+1; i++)
+            for (int j=lastY-1; j<=lastY+1; j++){
+                if (i<0 || i>9 || j<0 || j>9 || board.getSquare(i,j).getState().equals(Square.State.BLACK) || board.getSquare(i,j).getState().equals(Square.State.WHITE) ){
                     filled++;
-
-                } else if (board.getSquare(i,j).getState().equals(Square.State.EMPTY)) empty++;
+                } else if (board.getSquare(i,j).getState().equals(Square.State.EMPTY))
+                    empty++;
             }
 
+        // jezeli nie ma zadnego wolnego policz minmax dla wszystkich wolnych na planszy
         if (filled>0 && empty == 0) {
-
             for (int i = 0;i<10;i++)
                 for (int j=0;j<10;j++){
                     if (board.getSquare(i,j).getState().equals(Square.State.EMPTY)){
-
                         minimax = new MinMax();
                         miniMaxResult = minimax.MinaMaxAlgorithm(depth,i,j, this.checkValueOfArea(i,j,game.getBoard()), board, true);
                         if (miniMaxResult>=value){
-
                             value = miniMaxResult;
                             result[0] = i;
                             result[1] = j;
                         }
-
                     }
                 }
-
-        } else {
-
-        for (int i = lastX -1;i<=lastX + 1;i++)
-            for (int j = lastY -1; j<=lastY +1;j++ ){
-
-                if (i>=0 && i<10 && j>=0 && j<10){
-                    if (board.getSquare(i,j).getState().equals(Square.State.EMPTY)){
-
+        }
+        // w przeciwnym przypadku policz tylko dla wolnych w okolicy
+        else {
+            for (int i = lastX -1; i<=lastX + 1; i++)
+                for (int j = lastY -1; j<=lastY +1; j++ ){
+                    if (i>=0 && i<10 && j>=0 && j<10 && board.getSquare(i,j).getState().equals(Square.State.EMPTY)){
                         minimax = new MinMax();
-                    miniMaxResult = minimax.MinaMaxAlgorithm(depth,i,j, this.checkValueOfArea(i,j,game.getBoard()), board, true);
-                    if (miniMaxResult>=value){
-
-                        value = miniMaxResult;
-                        result[0] = i;
-                        result[1] = j;
+                        miniMaxResult = minimax.MinaMaxAlgorithm(depth,i,j, this.checkValueOfArea(i,j,game.getBoard()), board, true);
+                        if (miniMaxResult>=value){
+                            value = miniMaxResult;
+                            result[0] = i;
+                            result[1] = j;
+                        }
                     }
                 }
-                }
-
-            }
         }
         return result;
 
     }
-
 
 
 }
--- src/MinMax.java
+++ src/MinMax.java
@@ -17,62 +17,51 @@ public class MinMax {
         int empty = 0;
         int filled = 0;
 
-        for (int i = x-1;i<x+1;i++)
-            for (int j=y-1;j<y+1;j++){
-                if (i<0|| i>10 || j<0 || j>10 || board.getSquare(i,j).getState().equals(Square.State.BLACK) || board.getSquare(i,j).getState().equals(Square.State.WHITE) ){
-
-                filled++;
-
-                } else if (board.getSquare(i,j).getState().equals(Square.State.EMPTY)) empty++;
+        for (int i = x-1; i <= x+1; i++)
+            for (int j = y-1; j <= y+1; j++){
+                if (i<0 || i>9 || j<0 || j>9 || board.getSquare(i,j).getState().equals(Square.State.BLACK) || board.getSquare(i,j).getState().equals(Square.State.WHITE) ){
+                    filled++;
+                } else if (board.getSquare(i,j).getState().equals(Square.State.EMPTY)){
+                    empty++;
+                }
             }
 
         if (filled>0 && empty == 0) {
-
-            for (int i = 0;i<10;i++)
-                for (int j=0;j<10;j++){
-                        if (board.getSquare(i,j).getState().equals(Square.State.EMPTY)){
-
-                            row.add(new TreeNode(i, j, engine.checkValueOfArea(i, j, board) + weight));
-
-                        }
-                }
-
-        } else {
-
-
-        for (int i = x-1;i<x+1;i++)
-            for (int j=y-1;j<y+1;j++){
-                if (i>=0 && i<10 && j>=0 && j<10){
-                if (board.getSquare(i,j).getState().equals(Square.State.EMPTY)){
-
+            for (int i = 0; i<10; i++)
+                for (int j = 0; j < 10; j++){
+                    if (board.getSquare(i,j).getState().equals(Square.State.EMPTY)){
                         row.add(new TreeNode(i, j, engine.checkValueOfArea(i, j, board) + weight));
-
+                    }
                 }
-            }
-            }
+        } else {
+            for (int i = x-1; i <= x+1; i++)
+                for (int j = y-1; j <= y+1; j++){
+                    if (i>=0 && i<10 && j>=0 && j<10 && board.getSquare(i,j).getState().equals(Square.State.EMPTY)){
+                        row.add(new TreeNode(i, j, engine.checkValueOfArea(i, j, board) + weight));
+                    }
+                }
         }
         return row;
     }
 
 
 
-    public int MinaMaxAlgorithm(int depth, int x, int y, int weight, Board newBoard, boolean maxPlayer){
+    public int MinaMaxAlgorithm(int depth, int x, int y, int weight, Board board, boolean maxPlayer){
 
         int bestValue = 0;
         int value = 0;
-        Board newBoardB = newBoard;
 
-        if (depth == 0) return engine.checkValueOfArea(x, y, newBoardB);
+        if (depth == 0) return engine.checkValueOfArea(x, y, board);
         if (maxPlayer){
             bestValue= -1000;
-            for (TreeNode node: generateMove(x, y, newBoard, weight)){
-                value = MinaMaxAlgorithm(depth -1, node.getX(), node.getY(), node.getWeight(), newBoardB, false);
+            for (TreeNode node : generateMove(x, y, board, weight)){
+                value = MinaMaxAlgorithm(depth -1, node.getX(), node.getY(), node.getWeight(), board, false);
                 bestValue = Math.max(bestValue, value);
             }
         } else if (!maxPlayer){
             bestValue= 1000;
-            for (TreeNode node: generateMove(x, y, newBoardB, weight)){
-                value = MinaMaxAlgorithm(depth -1, node.getX(), node.getY(), node.getWeight(), newBoardB, true);
+            for (TreeNode node : generateMove(x, y, board, weight)){
+                value = MinaMaxAlgorithm(depth -1, node.getX(), node.getY(), node.getWeight(), board, true);
                 bestValue = Math.min(bestValue, value);
             }
         }
